3b3851601ee2ebd9e0b761cada245fcc5a267ed5
k.kruchanok@andersernlab.com <k.kruchanok@andersernlab.com>
2023-04-17 17:36:41 +0400

    Add two endpoints

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./App.module";
 
 const start = async () => {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
 
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
